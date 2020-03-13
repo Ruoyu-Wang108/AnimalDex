b3cb4b94cfdd723e4caf0a8f940af6302316d267
Shuhanstack <shuhan@bren.ucsb.edu>
2020-03-13 15:41:50 -0700

    removed some comments

--- server.R
+++ server.R
@@ -2,28 +2,6 @@
 shinyServer <- function(input, output, session) {
   
   
- 
-  
-  ## Interactive Map ###########################################
-  
-#   # Create the map
-#   output$map <- renderLeaflet({
-#     leaflet(nps_ca_five) %>%
-#       addProviderTiles(providers$Thunderforest.Outdoors) %>% 
-#       addTiles(
-#         urlTemplate = "https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=faa73f98b6a445298967f05e7a8908c4
-# ",
-#         attribution = "&copy; <a href=\"http://www.thunderforest.com/\">Thunderforest</a>, {attribution.OpenStreetMap}",
-#         options = tileOptions(variant='outdoors', apikey = 'faa73f98b6a445298967f05e7a8908c4')
-#       ) %>% 
-#       addPolygons(fill = FALSE, 
-#                   label = nps_ca_five$unit_name) %>% 
-#       setView(lng = -119, lat = 37.5, zoom = 5.5)
-#   })
-  
-  # California, Longitude: 36.7783° N, 119.4179° W
-  
-  
   # TAB 2---------------------------------------------
   # Reactive Data for parks
   
